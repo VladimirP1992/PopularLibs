f24ccb386d96ab8e0759fa6cd1eeb6d5b2ff13a0
VladimirP1992 <p.vladimir.m@yandex.ru>
2020-03-15 21:55:56 +0300

    Проект курса популярные библиотеки

---
+++ app/src/main/java/ru/geekbrains/popularlibs/ui/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package ru.geekbrains.popularlibs.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.geekbrains.popularlibs.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
