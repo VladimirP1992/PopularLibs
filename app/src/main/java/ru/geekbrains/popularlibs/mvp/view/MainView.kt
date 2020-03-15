2f54349cec509d2d4c05fae1e0a6418f40f44158
VladimirP1992 <p.vladimir.m@yandex.ru>
2020-03-15 22:27:59 +0300

    Интерфейс для слоя View с методами для каждой кнопки

---
+++ app/src/main/java/ru/geekbrains/popularlibs/mvp/view/MainView.kt
@@ -0,0 +1,7 @@
+package ru.geekbrains.popularlibs.mvp.view
+
+interface MainView {
+    fun setFistButtonText(text: String)
+    fun setSecondButtonText(text: String)
+    fun setThirdButtonText(text: String)
+}
