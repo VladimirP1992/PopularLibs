04b7d5152d5cdae0cc45bdd72c104a3bf33cac64
VladimirP1992 <p.vladimir.m@yandex.ru>
2020-03-19 00:58:22 +0300

    Репозиторий теперь Serializable

--- app/src/main/java/ru/geekbrains/popularlibs/mvp/model/entity/GithubRepository.kt
+++ app/src/main/java/ru/geekbrains/popularlibs/mvp/model/entity/GithubRepository.kt
@@ -1,8 +1,10 @@
 package ru.geekbrains.popularlibs.mvp.model.entity
 
+import java.io.Serializable
+
 //https://api.github.com/users/googlesamples/repos
-data class GithubRepository(
+data class GithubRepository (
     val id: String,
     val name: String,
     val forksCount: Int
-)
+): Serializable
