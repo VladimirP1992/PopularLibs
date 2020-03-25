3836b92b59bc9968ba0f3f40611b88e7b4ae5b35
VladimirP1992 <p.vladimir.m@yandex.ru>
2020-03-25 22:34:36 +0300

    Добавлен метод showUpdateError() для случая ошибки Observable().subscribe в RepositoriesPresenter

--- app/src/main/java/ru/geekbrains/popularlibs/mvp/view/RepositoriesView.kt
+++ app/src/main/java/ru/geekbrains/popularlibs/mvp/view/RepositoriesView.kt
@@ -8,4 +8,5 @@ import moxy.viewstate.strategy.StateStrategyType
 interface RepositoriesView : MvpView {
     fun init()
     fun updateList()
+    fun showUpdateError()
 }
--- app/src/main/java/ru/geekbrains/popularlibs/ui/fragments/RepositoriesFragment.kt
+++ app/src/main/java/ru/geekbrains/popularlibs/ui/fragments/RepositoriesFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.fragment_repositories.*
 import moxy.MvpAppCompatFragment
@@ -42,6 +43,10 @@ class RepositoriesFragment : MvpAppCompatFragment(), RepositoriesView, BackButto
 
     override fun updateList() {
         adapter?.notifyDataSetChanged()
+    }
+
+    override fun showUpdateError() {
+        Toast.makeText(activity, getString(R.string.error_repositories_update), Toast.LENGTH_SHORT).show()
     }
 
     override fun backClicked() = presenter.backClicked()
