2d67164520e62ab60f033f88f4432c6db09c2949
VladimirP1992 <p.vladimir.m@yandex.ru>
2020-03-25 22:31:19 +0300

    getRepos() теперь возвращает Observable

--- app/src/main/java/ru/geekbrains/popularlibs/mvp/model/repo/GithubRepositoriesRepo.kt
+++ app/src/main/java/ru/geekbrains/popularlibs/mvp/model/repo/GithubRepositoriesRepo.kt
@@ -1,15 +1,21 @@
 package ru.geekbrains.popularlibs.mvp.model.repo
 
+import io.reactivex.rxjava3.core.Observable
 import ru.geekbrains.popularlibs.mvp.model.entity.GithubRepository
 
 class GithubRepositoriesRepo {
 
-    val repositories = listOf(
+    private val repositories = listOf(
         GithubRepository("1", "name1", 100),
         GithubRepository("2", "name2", 200),
         GithubRepository("3", "name3", 300),
         GithubRepository("4", "name4", 400)
     )
 
-    fun getRepos() = repositories
+    //тут наверрное грузим репозитории из интернета
+    private fun loadRepositories() = repositories
+
+    fun getRepos() = Observable.fromCallable {
+        return@fromCallable loadRepositories()
+    }
 }
