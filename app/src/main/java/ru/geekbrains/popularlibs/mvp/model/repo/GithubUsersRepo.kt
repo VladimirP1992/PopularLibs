597ef0a3748933daaa50616b5e38b8a0d359dfd9
VladimirP1992 <p.vladimir.m@yandex.ru>
2020-04-02 15:36:39 +0300

    Пользователь и список его репозиториев возвращаются в виде Single полученных в результате Retrofit запроса

--- app/src/main/java/ru/geekbrains/popularlibs/mvp/model/repo/GithubUsersRepo.kt
+++ app/src/main/java/ru/geekbrains/popularlibs/mvp/model/repo/GithubUsersRepo.kt
@@ -3,6 +3,6 @@ package ru.geekbrains.popularlibs.mvp.model.repo
 import io.reactivex.rxjava3.schedulers.Schedulers
 import ru.geekbrains.popularlibs.mvp.model.api.IDataSource
 
-class GithubRepositoriesRepo (val api: IDataSource){
-    fun getRepos(reposUrl: String) = api.getRepos(reposUrl).subscribeOn(Schedulers.io())
+class GithubUsersRepo(val api: IDataSource) {
+    fun getUser(username: String) = api.getUser(username).subscribeOn(Schedulers.io())
 }
