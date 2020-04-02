24c63813e26b5b91d6240736f7fbbb58c650cc32
VladimirP1992 <p.vladimir.m@yandex.ru>
2020-04-02 15:25:08 +0300

    POJO классы для пользователя и репозитория

--- app/src/main/java/ru/geekbrains/popularlibs/mvp/model/entity/GithubRepository.kt
+++ app/src/main/java/ru/geekbrains/popularlibs/mvp/model/entity/GithubRepository.kt
@@ -1,12 +1,13 @@
 package ru.geekbrains.popularlibs.mvp.model.entity
 
 import android.os.Parcelable
+import com.google.gson.annotations.Expose
 import kotlinx.android.parcel.Parcelize
 
 //https://api.github.com/users/googlesamples/repos
 @Parcelize
 data class GithubRepository (
-    val id: String,
-    val name: String,
-    val forksCount: Int
+    @Expose val id: String,
+    @Expose val name: String,
+    @Expose val forksCount: Int
 ): Parcelable
