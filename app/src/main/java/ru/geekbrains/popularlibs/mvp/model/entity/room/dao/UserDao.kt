1cee499a05a97f039d68d1ab2efe5e62762d6b7c
VladimirP1992 <p.vladimir.m@yandex.ru>
2020-04-06 11:42:27 +0300

    Операции обращения в БД

---
+++ app/src/main/java/ru/geekbrains/popularlibs/mvp/model/entity/room/dao/UserDao.kt
@@ -0,0 +1,45 @@
+package ru.geekbrains.popularlibs.mvp.model.entity.room.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import ru.geekbrains.popularlibs.mvp.model.entity.room.RoomGithubUser
+
+@Dao
+interface UserDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(user: RoomGithubUser)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(vararg user: RoomGithubUser)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(users: List<RoomGithubUser>)
+
+    @Update
+    fun update(user: RoomGithubUser)
+
+    @Update
+    fun update(vararg user: RoomGithubUser)
+
+    @Update
+    fun update(users: List<RoomGithubUser>)
+
+    @Delete
+    fun delete(user: RoomGithubUser)
+
+    @Delete
+    fun delete(vararg user: RoomGithubUser)
+
+    @Delete
+    fun delete(users: List<RoomGithubUser>)
+
+    @Query("SELECT * FROM RoomGithubUser")
+    fun getAll(): List<RoomGithubUser>
+
+    @Query("SELECT * FROM RoomGithubUser WHERE login = :login LIMIT 1")
+    fun findByLogin(login: String): RoomGithubUser?
+}
