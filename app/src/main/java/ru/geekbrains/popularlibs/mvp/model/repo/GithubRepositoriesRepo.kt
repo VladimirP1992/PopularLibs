74fe273c21e8976573af437bd6f7df9642812f46
VladimirP1992 <p.vladimir.m@yandex.ru>
2020-04-06 15:51:16 +0300

    Вызовы методов проходят через методы интерфейса ...Cache

--- app/src/main/java/ru/geekbrains/popularlibs/mvp/model/repo/GithubRepositoriesRepo.kt
+++ app/src/main/java/ru/geekbrains/popularlibs/mvp/model/repo/GithubRepositoriesRepo.kt
@@ -1,39 +1,13 @@
 package ru.geekbrains.popularlibs.mvp.model.repo
 
-import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.schedulers.Schedulers
 import ru.geekbrains.popularlibs.mvp.model.api.IDataSource
-import ru.geekbrains.popularlibs.mvp.model.entity.GithubRepository
+import ru.geekbrains.popularlibs.mvp.model.cache.IRepositoriesCache
 import ru.geekbrains.popularlibs.mvp.model.entity.GithubUser
-import ru.geekbrains.popularlibs.mvp.model.entity.room.RoomGithubRepository
 import ru.geekbrains.popularlibs.mvp.model.entity.room.db.Database
 import ru.geekbrains.popularlibs.mvp.model.network.NetworkStatus
-import java.lang.RuntimeException
 
 //TODO: Практическое задание 1 - вытащить кэширование в отдельный класс RoomRepositoriesCache и внедрить его сюда через интфейс IRepositoriesCache
-class GithubRepositoriesRepo(val api: IDataSource, val networkStatus: NetworkStatus, val database: Database) {
-
-    fun getUserRepositories(user: GithubUser) = networkStatus.isOnlineSingle().flatMap { isOnline ->
-        if (isOnline) {
-            api.getUserRepos(user.reposUrl)
-                .map { repos ->
-                    repos.takeIf { it.isNotEmpty() }?.let {
-                        val roomRepos = repos.map { RoomGithubRepository(it.id, it.name, it.forksCount, user.login, it.language) }
-                        database.repositoryDao.insert(roomRepos)
-                    }
-                    repos
-                }
-        } else {
-            Single.create<List<GithubRepository>> { emitter ->
-                database.userDao.findByLogin(user.login)?.let { roomUser ->
-                    val roomRepos = database.repositoryDao.findForUser(user.login)
-                    val repos = roomRepos.map { GithubRepository(it.id, it.name, it.forksCount, it.language ?: "") }
-                    emitter.onSuccess(repos)
-                } ?: let {
-                    emitter.onError(RuntimeException("No such user in cache"))
-                }
-            }
-        }
-    }.subscribeOn(Schedulers.io())
-
+class GithubRepositoriesRepo(val api: IDataSource, val networkStatus: NetworkStatus, val database: Database, val repositoriesCache: IRepositoriesCache) {
+    fun getUserRepositories(user: GithubUser) = repositoriesCache.getUserRepositories(user, api, networkStatus, database).subscribeOn(Schedulers.io())
 }
--- app/src/main/java/ru/geekbrains/popularlibs/mvp/model/repo/GithubUsersRepo.kt
+++ app/src/main/java/ru/geekbrains/popularlibs/mvp/model/repo/GithubUsersRepo.kt
@@ -1,35 +1,13 @@
 package ru.geekbrains.popularlibs.mvp.model.repo
 
-import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.schedulers.Schedulers
 import ru.geekbrains.popularlibs.mvp.model.api.IDataSource
-import ru.geekbrains.popularlibs.mvp.model.entity.GithubUser
-import ru.geekbrains.popularlibs.mvp.model.entity.room.RoomGithubUser
+import ru.geekbrains.popularlibs.mvp.model.cache.IUserCache
 import ru.geekbrains.popularlibs.mvp.model.entity.room.db.Database
 import ru.geekbrains.popularlibs.mvp.model.network.NetworkStatus
-import java.lang.RuntimeException
+
 
 //TODO: Практическое задание 1 - вытащить кэширование в отдельный класс RoomUserCache и внедрить его сюда через интфейс IUserCache
-class GithubUsersRepo(val api: IDataSource, val networkStatus: NetworkStatus, val database: Database) {
-    fun getUser(username: String) = networkStatus.isOnlineSingle().flatMap { isOnline ->
-        if (isOnline) {
-            api.getUser(username)
-                .map { user ->
-                    val roomUser = database.userDao.findByLogin(username)?.apply {
-                        avatarUrl = user.avatarUrl
-                        reposUrl = user.reposUrl
-                    } ?: RoomGithubUser(user.login, user.avatarUrl, user.reposUrl)
-                    database.userDao.insert(roomUser)
-                    user
-                }
-        } else {
-            Single.create<GithubUser> { emitter ->
-                database.userDao.findByLogin(username)?.let { roomUser ->
-                    emitter.onSuccess(GithubUser(roomUser.login, roomUser.avatarUrl, roomUser.reposUrl))
-                } ?: let {
-                    emitter.onError(RuntimeException("No such user in cache"))
-                }
-            }
-        }
-    }.subscribeOn(Schedulers.io())
+class GithubUsersRepo(val api: IDataSource, val networkStatus: NetworkStatus, val database: Database, val userCache: IUserCache) {
+    fun getUser(username: String) = userCache.getUser(username, api, networkStatus, database).subscribeOn(Schedulers.io())
 }
