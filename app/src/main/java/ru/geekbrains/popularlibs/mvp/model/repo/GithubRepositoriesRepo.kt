8a9499952c5323f11678e970451bda154de6bc76
VladimirP1992 <p.vladimir.m@yandex.ru>
2020-04-09 14:15:28 +0300

    Получение узера/репозиториев с учетом состояния сети

--- app/src/main/java/ru/geekbrains/popularlibs/mvp/model/repo/GithubRepositoriesRepo.kt
+++ app/src/main/java/ru/geekbrains/popularlibs/mvp/model/repo/GithubRepositoriesRepo.kt
@@ -2,11 +2,21 @@ package ru.geekbrains.popularlibs.mvp.model.repo
 
 import io.reactivex.rxjava3.schedulers.Schedulers
 import ru.geekbrains.popularlibs.mvp.model.api.IDataSource
-import ru.geekbrains.popularlibs.mvp.model.cache.IRepositoriesCache
+import ru.geekbrains.popularlibs.mvp.model.cache.IGithubRepositoriesCache
 import ru.geekbrains.popularlibs.mvp.model.entity.GithubUser
-import ru.geekbrains.popularlibs.mvp.model.entity.room.db.Database
 import ru.geekbrains.popularlibs.mvp.model.network.NetworkStatus
 
-class GithubRepositoriesRepo(val api: IDataSource, val networkStatus: NetworkStatus, val database: Database, val repositoriesCache: IRepositoriesCache) {
-    fun getUserRepositories(user: GithubUser) = repositoriesCache.getUserRepositories(user, api, networkStatus, database).subscribeOn(Schedulers.io())
+class GithubRepositoriesRepo(val api: IDataSource, val networkStatus: NetworkStatus, val cache: IGithubRepositoriesCache) {
+
+    fun getUserRepositories(user: GithubUser) = networkStatus.isOnlineSingle().flatMap { isOnline ->
+        if (isOnline) {
+            api.getUserRepos(user.reposUrl)
+                .flatMap { repos ->
+                    cache.putUserRepos(user, repos).toSingleDefault(repos)
+                }
+        } else {
+            cache.getUserRepos(user)
+        }
+    }.subscribeOn(Schedulers.io())
+
 }
--- app/src/main/java/ru/geekbrains/popularlibs/mvp/model/repo/GithubUsersRepo.kt
+++ app/src/main/java/ru/geekbrains/popularlibs/mvp/model/repo/GithubUsersRepo.kt
@@ -2,10 +2,18 @@ package ru.geekbrains.popularlibs.mvp.model.repo
 
 import io.reactivex.rxjava3.schedulers.Schedulers
 import ru.geekbrains.popularlibs.mvp.model.api.IDataSource
-import ru.geekbrains.popularlibs.mvp.model.cache.IUserCache
-import ru.geekbrains.popularlibs.mvp.model.entity.room.db.Database
+import ru.geekbrains.popularlibs.mvp.model.cache.IGithubUsersCache
 import ru.geekbrains.popularlibs.mvp.model.network.NetworkStatus
 
-class GithubUsersRepo(val api: IDataSource, val networkStatus: NetworkStatus, val database: Database, val userCache: IUserCache) {
-    fun getUser(username: String) = userCache.getUser(username, api, networkStatus, database).subscribeOn(Schedulers.io())
+class GithubUsersRepo(val api: IDataSource, val networkStatus: NetworkStatus, val cache: IGithubUsersCache) {
+    fun getUser(username: String) = networkStatus.isOnlineSingle().flatMap { isOnline ->
+        if (isOnline) {
+            api.getUser(username)
+                .flatMap { user ->
+                    return@flatMap cache.putUser(user).toSingleDefault(user)
+                }
+        } else {
+            cache.getUser(username)
+        }
+    }.subscribeOn(Schedulers.io())
 }
